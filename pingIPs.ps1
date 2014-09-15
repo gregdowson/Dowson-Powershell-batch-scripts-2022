c14c615000592ba09ba057a258a16a5a0b590638
Gregory Dowson <gregdowson@comcast.net>
2014-09-15 16:56:56 -0700

    Create pingIPs.ps1

---
+++ pingIPs.ps1
@@ -0,0 +1,15 @@
+$ServerName = Get-Content "c:\scripts\Computers2.txt"  
+  
+foreach ($Server in $ServerName) {  
+  
+        if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {   
+          
+            "$Server is Pinging "  
+          
+                    } else  
+                      
+                    {"$Server not pinging"  
+              
+                    }      
+          
+} 
