158003cfb5978135c695eba0c5e7ab8fec56180e
Gregory Dowson <gregdowson@comcast.net>
2017-10-19 13:12:36 -0700

    Delete ADUpdate_Template - Copy.ps1

--- ADUpdate_Template - Copy.ps1
+++
@@ -1,5 +0,0 @@
-$users = Get-Content "C:\list.txt"          
-            
-foreach ($user in $users) {            
-Set-ADUser -Identity $user -Replace @{Manager = "CN=Jsmith,OU=IT,OU=AllUsers,DC=mydomain,DC=local"
-}}
