cb3ff711a0dc354b3af349889a3966be1894b33b
Gregory Dowson <gregdowson@comcast.net>
2017-10-19 13:15:59 -0700

    Delete delete_files 2 - Copy.ps1

--- delete_files 2 - Copy.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem -Path  '\\nmi.local\files\NMIDocs\*\AppData\Roaming\Mozilla\Firefox\Profiles\*' -Recurse -exclude 'places.sqlite' |
-Select -ExpandProperty FullName |
-Where {$_ -notlike '\\nmi.local\files\NMIDocs\*\AppData\Roaming\Mozilla\Firefox\Profiles\*\*'} |
-sort length -Descending |
-Remove-Item -force -confirm
-
-
-
