b5a1c3d66d5ac6af9c1135b699ed99f3326d6cff
Gregory Dowson <gregdowson@comcast.net>
2017-10-23 10:47:31 -0700

    Update ip_addresses.ps1

--- ip_addresses.ps1
+++ ip_addresses.ps1
@@ -1,6 +1,6 @@
 $servers = get-content "c:\scripts\servers.txt"
 foreach ($server in $servers) {
-  $addresses = [System.Net.Dns]::GetHostAddresses($server)
+  $addresses = [System.Net.Dns]::GetHostAddresses($server)}
   foreach($a in $addresses) {
-    "{0},{1}" -f $server, $a.IPAddressToString
- } |Export-Csv c:\scripts\ips.txt -force
+    "{0},{1}" -f $server, $a.IPAddressToString 
+  Export-Csv c:\scripts\ips.txt -force}
