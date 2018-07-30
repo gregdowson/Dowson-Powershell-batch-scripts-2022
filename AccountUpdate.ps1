3dffcb154a7e446d1e0754653067f7efbd1821a4
Gregory Dowson <gregdowson@comcast.net>
2018-07-30 11:24:06 -0700

    Add files via upload

---
+++ AccountUpdate.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module ActiveDirectory  
+
+$users = Import-Csv "C:\Scripts\LCLAdmin Groups\ADusers.csv"
+foreach ($user in $users) {
+Get-ADUser -Filter "samaccountname -eq '$($user.samaccountname)'" -Properties * -SearchBase "DC=ds,DC=usfca,DC=edu"|
+Set-ADUser -givenname $($user.firstname) -surname $($user.surname)}
