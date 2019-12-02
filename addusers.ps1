2dea9d54c8a1ba5ae32466019fd128263f9e9e11
Gregory Dowson <gregdowson@comcast.net>
2019-12-02 11:07:09 -0800

    Add files via upload

---
+++ addusers.ps1
@@ -0,0 +1,22 @@
+﻿$Servers = Get-Content "C:\users\gdowson-a\documents\servers.txt"
+$Users = get-content "C:\users\gdowson-a\documents\VMSVF26 admins.txt"
+$GroupName = "VMSVJ78\Adminsistrators"
+$DomainName = $env:USERDOMAIN
+$ErrorActionPreference = "Stop"
+ 
+Foreach($Server in $Servers) {
+    $Server = $Server.trim()
+    $ComputerName = $Server
+    Write-Host "Processing $ComputerName" -ForegroundColor Green
+    Foreach($Username in $Users){
+        Try{
+            $Group = [ADSI]"WinNT://$ComputerName/$GroupName,group"
+            $User = [ADSI]"WinNT://$DomainName/$Username,user"
+            $Group.Add($User.Path)
+        }
+        Catch{
+            $_.Exception.innerexception
+            Continue
+        }
+    }
+}
---
+++ get Remote local group users Remote.ps1
@@ -0,0 +1,15 @@
+﻿function get-localadmin { 
+param ($strcomputer) 
+ 
+$admins = Gwmi win32_groupuser –computer $strcomputer  
+$admins = $admins |? {$_.groupcomponent –like '*"Remote Desktop Users"'} 
+ 
+$admins |% { 
+$_.partcomponent –match “.+Domain\=(.+)\,Name\=(.+)$” > $nul 
+$matches[1].trim('"') + “\” + $matches[2].trim('"') 
+} 
+}
+
+$Servers = Get-Content C:\scripts\servers.txt
+Foreach($Server in $Servers) {get-localadmin $Server}
+   
